1612a1c637ddb5176c7d4eec07c02fdc2591d25e
James Utley <james.utley@syndicate-labs.io>
2024-08-29 09:35:07 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,5 @@
+# Import SmartScraperGraph from scrapegraphai.graphs module
+from scrapegraphai.graphs import SmartScraperGraph
+import nest_asyncio   # Import nest_asyncio module for asynchronous operations
+nest_asyncio.apply()   # Apply nest_asyncio to resolve any issues with asyncio event
+
